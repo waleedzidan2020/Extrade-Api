2a0ae344836c96ca2d4dd7c042c16d8bc6b760ad
Mohamed Abdelkader <mo7amad1472@gmail.com>
2022-07-22 11:00:03 +0200

    CollectionCreate

--- extrade api/src/app/appRouting.ts
+++ extrade api/src/app/appRouting.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { LogInComponent } from "./Account/log-in/log-in.component";
+import { CollectionComponent } from "./Component/collection/collection.component";
 import { ResultComponent } from "./result/result.component";
 import { UserComponent } from "./User/user.component";
 
@@ -15,6 +16,9 @@ let myroutes=[
     },
     {
         path:"SignUp",component:UserComponent
+    },
+    {
+        path:"CreateCollection",component:CollectionComponent
     }
 ]
 
